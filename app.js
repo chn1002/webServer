62d514325e7797501c52f01315cca3b774ca1dab
jihun chon <chn1002@gmail.com>
2021-01-14 15:21:43 +0900

    Sample Web for Node.JS

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var compression = require('compression');
+var app = express();
+ 
+app.use(compression());
+app.use(express.static(__dirname + '/public/'));
+ 
+app.listen(80, function(){
+    console.log("webserver listening!");
+});
